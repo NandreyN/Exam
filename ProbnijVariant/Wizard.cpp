45d1ac28ef25d0d5b86db26d443f16db877e8109
NandreyN <nandreyn1998@gmail.com>
2017-01-13 23:06:32 +0300

    ff

--- ProbnijVariant/Wizard.cpp
+++ ProbnijVariant/Wizard.cpp
@@ -1,1 +1,29 @@
 #include "Wizard.h"
+
+Wizard::Wizard(Sex s, Race r, unsigned int hp, unsigned int mn) :_maxMana(mn), Character(s, r, hp)
+{
+	this->_mana = mn;
+}
+
+void Wizard::heal(Character& target, int h)
+{
+	if (!isEnoughMana(h))
+		return;
+	target.setHealth(target.getCurrentHealth() + h);
+	this->_mana -= h * 2;
+}
+
+void Wizard::heal(Wizard& target, int h)
+{
+	if (!isEnoughMana(h))
+		return;
+	target.setHealth(target.getCurrentHealth() + h);
+	this->_mana -= h * 2;
+}
+
+bool Wizard::isEnoughMana(int needToHealHP) const
+{
+	if (this->_mana * 2 >= needToHealHP)
+		return true;
+	return false;
+}
--- ProbnijVariant/Wizard.h
+++ ProbnijVariant/Wizard.h
@@ -4,13 +4,13 @@ class Wizard :
 	public Character
 {
 public:
-	Wizard();
+	Wizard(Sex, Race, unsigned int, unsigned int);
 	void heal(Character&, int);
 	void heal(Wizard&, int);
 private:
 	unsigned const int _maxMana;
 	unsigned int _mana;
-	bool isEnoughMana() const;
+	bool isEnoughMana(int) const;
 };
 
 std::ofstream operator<<(std::ofstream&, Wizard&);
